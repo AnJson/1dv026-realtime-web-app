9f990128c3856bc7e301a62e3f7d91b7c7e87dfc
Anders Jonsson <aj224rj@student.lnu.se>
2022-02-24 14:01:51 +0100

    Add initial controllers for editing issue

--- src/controllers/issues-controller.js
+++ src/controllers/issues-controller.js
@@ -15,7 +15,7 @@ import fetch from 'node-fetch'
 export class IssuesController {
   /**
    * Renders a view and sends the rendered HTML string as an HTTP response.
-   * Uses possible query-parameters to filter result.
+   * Fetches issues from gitlab and sends data to view.
    * (GET '/issues').
    *
    * @param {object} req - Express request object.
@@ -50,6 +50,53 @@ export class IssuesController {
       res.render('issues/index', { viewData })
     } catch (error) {
       next(error)
+    }
+  }
+
+  /**
+   * Renders a view and sends the rendered HTML string as an HTTP response.
+   * Fetches issue from gitlab and sends to view.
+   * (GET '/edit/:id').
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async edit (req, res, next) {
+    try {
+      // TODO: Fetch issue and map viewData.
+
+      /* const response = await fetch(process.env.ISSUES_URL, {
+        headers: {
+          authorization: `Bearer ${process.env.AUTHENTICATION_TOKEN}`
+        }
+      }) */
+
+      const viewData = null // NOTE: Fix viewData
+
+      res.render('issues/edit', { viewData })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Updates the issue on gitlab and redirects to home.
+   * (POST '/edit/:id').
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async editPost (req, res, next) {
+    try {
+      // TODO: Update the post and use PRG-pattern.
+
+      req.session.flash = { type: 'success', text: 'Successfully updated issue.' }
+      res.redirect('.') // NOTE: Check the redirect.
+    } catch (error) {
+      req.session.flash = { type: 'error', text: error.message }
+      res.redirect('.')
     }
   }
 }
