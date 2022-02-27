612a0e543a3e8e6ac240131c643a79c703d24fcd
Anders Jonsson <aj224rj@student.lnu.se>
2022-02-27 22:15:15 +0100

    Fix bug in client-js to use socket even in subdirectories

--- public/js/index.js
+++ public/js/index.js
@@ -4,14 +4,20 @@
  * @author Anders Jonsson
  * @version 1.0.0
  */
-import '../socket.io/socket.io.js'
 import { addIssue } from './add-issue.js'
 
 const issueTemplate = document.querySelector('#issue-template')
 
 if (issueTemplate) {
+  await import('../socket.io/socket.io.js')
   // Create a socket connection using Socket.IO.
-  const socket = window.io()
+  const base = document.querySelector('base')
+  const path = base
+    ? (new URL('socket.io', base.href)).pathname
+    : '/socket.io'
+
+  const socket = window.io.connect('/', { path })
+
   // Listen for "issue/create" message from the server.
   socket.on('issue/create', (issue) => addIssue(issue, issueTemplate))
 }
